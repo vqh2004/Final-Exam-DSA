ab19d637aa0c1c64eb25625070a500406e19ee88
Minh Hằng Nguyễn thị <nminhhang149@e.com>
2023-12-16 02:13:51 +0700

    Dijkstra.java

--- BICYCLE_MANAGEMENT/Dijkstra.java
+++ BICYCLE_MANAGEMENT/Dijkstra.java
@@ -3,65 +3,68 @@ package BICYCLE_MANAGEMENT;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.PriorityQueue;
 
 public class Dijkstra {
-    void findTheShortestPath(Vertex vertex) {
-        vertex.setDistance(0);
-        vertex.setVisited(true);
+//    void findTheShortestPath(Vertex vertex) {
+//        vertex.setDistance(0);
+//        vertex.setVisited(true);
+//
+//        PriorityQueue<Vertex> vertices = new PriorityQueue<>();
+//        vertices.add(vertex);
+//
+//        while (!vertices.isEmpty()) {
+//            Vertex top = vertices.poll();
+//            for (Edge e : top.getAdjEdge()) {
+//                Vertex endV = e.getEndPoint();
+//
+//                if (!endV.isVisited()) {
+//                    double newDist = top.getDistance() + e.getDist();
+//                    if (newDist < endV.getDistance()) {
+//                        vertices.remove(endV);
+//                        endV.setDistance(newDist);
+//                        endV.setPrevious(top);
+//                        vertices.add(endV);
+//                    }
+//                }
+//            }
+//            top.setVisited(true);
+//        }
+//    }
 
-        PriorityQueue<Vertex> vertices = new PriorityQueue<>();
-        vertices.add(vertex);
-
-        while (!vertices.isEmpty()) {
-            Vertex top = vertices.poll();
-            for (Edge e : top.getAdjEdge()) {
-                Vertex endV = e.getEndPoint();
-
-                if (!endV.isVisited()) {
-                    double newDist = top.getDistance() + e.getDist();
-                    if (newDist < endV.getDistance()) {
-                        vertices.remove(endV);
-                        endV.setDistance(newDist);
-                        endV.setPrevious(top);
-                        vertices.add(endV);
-                    }
-                }
-            }
-            top.setVisited(true);
-        }
-    }
-
+    //tìm trạm gần nhất với vị trí của khách hàng (vertex)
     Vertex findTheNearestStation(Vertex vertex) {
         vertex.setDistance(0);
-        vertex.setVisited(true);
 
-        PriorityQueue<Vertex> vertices = new PriorityQueue<>();
-        vertices.add(vertex);
+        SortedPriorityQueue vertices = new SortedPriorityQueue();
+        vertices.insert(vertex);
 
         while (!vertices.isEmpty()) {
-            Vertex top = vertices.poll();
-            for (Edge e : top.getAdjEdge()) {
+            Vertex top = vertices.removeMin();
+            for (Edge e : top.getAdjEdge()) { // xét các cạnh kề với đỉnh top
                 Vertex endV = e.getEndPoint();
                 if (!endV.isVisited()) {
+                    //distance là khoảng cách từ đỉnh vertex đến đỉnh endV
+                    // dist là trọng số của cạnh có đỉnh đầu là top và đỉnh cuối là endV
                     double newDist = top.getDistance() + e.getDist();
                     if (newDist < endV.getDistance()) {
-//                        vertices.remove(endV);
                         endV.setDistance(newDist);
                         endV.setPrevious(top);
-                        vertices.add(endV);
+                        vertices.insert(endV);
                     }
                 }
+//                printPriority(vertices);
             }
-            if(!vertices.isEmpty() && vertices.peek().isRealStation()){
-                return vertices.peek();
+            if(vertices.min().isRealStation()){
+                return vertices.min();
             }
             top.setVisited(true);
         }
         return null;
     }
 
-
+    public void printPriority(SortedPriorityQueue vertices){
+        vertices.print();
+    }
 
     public List<Vertex> getTheShortestPath(Vertex vertex) {
         List<Vertex> vertexList = new ArrayList<>();
@@ -81,4 +84,5 @@ public class Dijkstra {
         }
         System.out.print(vertex.getName());
     }
+
 }
